2ba68e97d5b9e3cb195552505fd0e01c2103255f
1103SCORPIO <115768696+1103SCORPIO@users.noreply.github.com>
2022-10-15 10:09:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 冯夺利 2022年10月14日 https://github.com/1103SCORPIO
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
